1094e7192cd84cc15cc8183b3edd4d48311cf912
osose <ososesamuel2@Gmail.com>
2025-02-21 04:44:26 -0800

    initiak commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import App from "./App"; // Ensure this path is correct
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
